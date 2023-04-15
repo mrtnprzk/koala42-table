66a7e87628c18c414190364986bbde7bc0b0182b
mrtnprzk <marti.parizek@gmail.com>
2023-04-15 15:57:39 +0200

    [ADD] prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <main className="">
-      <h1 className='text-red-500'>Hello KOALA42!</h1>
-    </main>
-  )
+    return (
+        <main className="">
+            <h1 className="text-red-500">Hello KOALA42!</h1>
+        </main>
+    );
 }
